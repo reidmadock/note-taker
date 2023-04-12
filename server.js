9440e4428630adb8f920054b7c5c91f94f3cd6b4
Reid M <reidmadock@gmail.com>
2023-04-11 21:37:44 -0500

    removed logs, added some comments

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ app.use(express.json()); // parsing data from POST requires this line
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/notes', (req, res) => {
-    // res.json(`${req.method} test request received`);
-    console.info(`${req.method} test log to server`)
+    // change to notes.html webpage
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    // console.info(`${req.method} test api path from server`);
-
+    // read in file from the json acting as our database, send back the data as a string
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) {
             console.error(err);
@@ -31,11 +29,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // res.json(`${req.method} test post received`);
-    console.info(`${req.method} Log data to server console`)
-
+    // deconstruct variables out of the request body
     const {title, text} = req.body;
 
+    /* check to see if the fields were populated, even though they should be
+    as the frontend is set not to show a save button if the fields are empty */
     if(title && text) {
         const newNote = {
             title,
@@ -48,7 +46,7 @@ app.post('/api/notes', (req, res) => {
                 console.error(err);
             } else {
                 const allNotes = JSON.parse(data);
-
+                // add new entry into the object array
                 allNotes.push(newNote);
 
                 fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 4), (err) => {
@@ -71,18 +69,16 @@ app.post('/api/notes', (req, res) => {
 /* This took forever to fix, turns out it's cause in the db I had id called "note_id" but the provided index
 file only parsed for "id" when making the request therefor the requests were empty */
 app.delete('/api/notes/:id', (req, res) => {
-    // const id = req.params.id;
     const curId = req.params.id;
-    
+    // make sure an ID was sent with DELETE request
     if (curId) {
         fs.readFile('./db/db.json', 'utf-8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 let allNotes = JSON.parse(data);
-                // console.info(allNotes);
+
                 allNotes = allNotes.filter(({id}) => id !== curId);
-                // console.info(allNotes);
                 
                 fs.writeFile('./db/db.json', JSON.stringify(allNotes, null, 4), (err) => {
                     err ? console.error('Error writing', err) : console.info('Delete/write successful');
